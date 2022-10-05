bb8923376fd034bcaf2faed1d3c703bb82e4fae2
gabrieldiassales <gabrieldiassales@gmail.com>
2022-10-05 19:30:09 -0300

    Terceiro commit

--- script.js
+++ script.js
@@ -7,6 +7,24 @@ snake[0] = {
   y: 8 * box
 }
 let direction = "right";
+let food = {
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box
+}
+
+document.addEventListener('keydown', update);
+
+function update(event){
+  if (event.keyCode == 37 && direction != "right") direction = "left";
+  if (event.keyCode == 38 && direction != "down") direction = "up";
+  if (event.keyCode == 39 && direction != "left") direction = "right";
+  if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+function drawFood(){
+  context.fillStyle = "red";
+  context.fillRect(food.x, food.y, box, box);
+}
 
 function criarBG(){
   context.fillStyle = "lightgreen";
@@ -22,7 +40,13 @@ function criarCobrinha(){
 }
 
 function iniciarJogo(){
+  if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0; 
+  if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+  if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+  if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
   criarBG();
+  drawFood();
   criarCobrinha();
 
   let snakeX = snake[0].x;
@@ -43,4 +67,4 @@ function iniciarJogo(){
   snake.unshift(newHead);
 }
 
-let jogo = setInterval(iniciarJogo, 150);
+let jogo = setInterval(iniciarJogo, 100);
